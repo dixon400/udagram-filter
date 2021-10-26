895fcbc2fed0a687257ff9f0f84d4af5d1553da3
Muyideen Khalifa <dixonmk400@gmail.com>
2021-10-26 10:22:04 +0100

    New Api url for the frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a238b87209e8c4610b9fa6f73767442e-1374402093.us-east-1.elb.amazonaws.com:80/api/v0'
 };
 
 /*
